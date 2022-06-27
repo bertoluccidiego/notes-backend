9d64db7962e8ccf7459dbc67bf994f9cdb68cdcd
Diego C. Bertolucci <bertolucci.diego.c@gmail.com>
2022-06-27 17:14:36 -0300

    Eslint and prettier added and configured

--- index.js
+++ index.js
@@ -1,36 +1,55 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 function requestLogger(request, response, next) {
-  console.log('Method', request.method);
-  console.log('Path', request.path);
-  console.log('Body', request.body);
-  console.log('---');
+  console.log("Method", request.method);
+  console.log("Path", request.path);
+  console.log("Body", request.body);
+  console.log("---");
 
   next();
 }
 
 function unknownEndpoint(request, response) {
-  response.status(404).json({ error: 'unknown endpoint' });
+  response.status(404).json({ error: "unknown endpoint" });
 }
 
 app.use(express.json());
 app.use(requestLogger);
 
-let notes = [  {    id: 1,    content: "HTML is easy",    date: "2022-05-30T17:30:31.098Z",    important: true  },  {    id: 2,    content: "Browser can execute only Javascript",    date: "2022-05-30T18:39:34.091Z",    important: false  },  {    id: 3,    content: "GET and POST are the most important methods of HTTP protocol",    date: "2022-05-30T19:20:14.298Z",    important: true  }]
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2022-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2022-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2022-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
 
-app.get('/', (request, response) => {
-  response.end('<h1>Hello World</h1>');
+app.get("/", (request, response) => {
+  response.end("<h1>Hello World</h1>");
 });
 
-app.get('/api/notes', (request, response) => {
+app.get("/api/notes", (request, response) => {
   response.json(notes);
 });
 
-app.get('/api/notes/:id', (request, response) => {
+app.get("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
+  const note = notes.find((n) => n.id === id);
 
   if (note) {
     response.json(note);
@@ -39,18 +58,18 @@ app.get('/api/notes/:id', (request, response) => {
   }
 });
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
   notes = notes.filter((note) => note.id !== id);
 
   response.status(204).end();
 });
 
-app.post('/api/notes', (request, response) => {
+app.post("/api/notes", (request, response) => {
   const { body } = request;
 
   if (!body.content) {
-    return response.status(400).json({ error: 'content missing' });
+    return response.status(400).json({ error: "content missing" });
   }
 
   const note = {
