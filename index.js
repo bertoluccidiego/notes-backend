8a74f360a3cd43bfc06a53dfa8128436818b5ff3
Diego C. Bertolucci <bertolucci.diego.c@gmail.com>
2022-06-29 16:21:42 -0300

    Deploy to Heroku

--- index.js
+++ index.js
@@ -1,53 +1,55 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 function requestLogger(request, response, next) {
-  console.log("Method", request.method);
-  console.log("Path", request.path);
-  console.log("Body", request.body);
-  console.log("---");
+  console.log('Method', request.method);
+  console.log('Path', request.path);
+  console.log('Body', request.body);
+  console.log('---');
 
   next();
 }
 
 function unknownEndpoint(request, response) {
-  response.status(404).json({ error: "unknown endpoint" });
+  response.status(404).json({ error: 'unknown endpoint' });
 }
 
 app.use(express.json());
 app.use(requestLogger);
+app.use(cors());
 
 let notes = [
   {
     id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
+    content: 'HTML is easy',
+    date: '2022-05-30T17:30:31.098Z',
     important: true,
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
+    content: 'Browser can execute only Javascript',
+    date: '2022-05-30T18:39:34.091Z',
     important: false,
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2022-05-30T19:20:14.298Z',
     important: true,
   },
 ];
 
-app.get("/", (request, response) => {
-  response.end("<h1>Hello World</h1>");
+app.get('/', (request, response) => {
+  response.end('<h1>Hello World</h1>');
 });
 
-app.get("/api/notes", (request, response) => {
+app.get('/api/notes', (request, response) => {
   response.json(notes);
 });
 
-app.get("/api/notes/:id", (request, response) => {
+app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id);
   const note = notes.find((n) => n.id === id);
 
@@ -58,18 +60,18 @@ app.get("/api/notes/:id", (request, response) => {
   }
 });
 
-app.delete("/api/notes/:id", (request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id);
   notes = notes.filter((note) => note.id !== id);
 
   response.status(204).end();
 });
 
-app.post("/api/notes", (request, response) => {
+app.post('/api/notes', (request, response) => {
   const { body } = request;
 
   if (!body.content) {
-    return response.status(400).json({ error: "content missing" });
+    return response.status(400).json({ error: 'content missing' });
   }
 
   const note = {
@@ -80,12 +82,12 @@ app.post("/api/notes", (request, response) => {
   };
   notes = notes.concat(note);
 
-  response.json(note);
+  return response.json(note);
 });
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
