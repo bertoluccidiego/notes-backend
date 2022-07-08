c00b08a250c4877aae521444e5cdf10ab789127a
Diego C. Bertolucci <bertolucci.diego.c@gmail.com>
2022-07-08 15:13:25 -0300

    Error handling and other operations
      - Added error handler as middleware for malformatted id errors
      - Edited the delete route to integrate with MongoDB
      - Created a put route to edit notes integrated with MongoDB

--- index.js
+++ index.js
@@ -19,31 +19,20 @@ function unknownEndpoint(request, response) {
   response.status(404).json({ error: 'unknown endpoint' });
 }
 
-app.use(express.json());
-app.use(requestLogger);
-app.use(cors());
-app.use(express.static('build'));
+function errorHandler(error, request, response, next) {
+  console.error(error.message);
 
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2022-05-30T17:30:31.098Z',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2022-05-30T18:39:34.091Z',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2022-05-30T19:20:14.298Z',
-    important: true,
-  },
-];
+  if (error.name === 'CastError') {
+    return response.status(400).json({ error: 'malformatted id' });
+  }
+
+  next(error);
+}
+
+app.use(express.static('build'));
+app.use(express.json());
+app.use(cors());
+app.use(requestLogger);
 
 app.get('/', (request, response) => {
   response.end('<h1>Hello World</h1>');
@@ -55,17 +44,28 @@ app.get('/api/notes', (request, response) => {
   });
 });
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      }
+
+      return response.status(404).end();
+    })
+    .catch((error) => {
+      next(error);
+      console.log(error);
+      response.status(400).json({ error: 'malformatted id' });
+    });
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 // eslint-disable-next-line
@@ -87,7 +87,23 @@ app.post('/api/notes', (request, response) => {
   });
 });
 
+app.put('/api/notes/:id', (request, response, next) => {
+  const { body } = request;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
 app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const { PORT } = process.env;
 app.listen(PORT, () => {
