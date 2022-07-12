ded86bce2352aa90357efaa55d0886dc0d2591ae
Diego C. Bertolucci <bertolucci.diego.c@gmail.com>
2022-07-12 15:32:15 -0300

    feat: basic testing with jest added

---
+++ utils/for_testing.js
@@ -0,0 +1,16 @@
+function reverse(string) {
+  return string.split('').reverse().join('');
+}
+
+function average(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+
+  return array.reduce((sum, item) => sum + item, 0) / array.length;
+}
+
+module.exports = {
+  reverse,
+  average,
+};
